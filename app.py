f973991a9f90f5aa59bda98dc389fdcbffed50fa
Nguyên Vũ <83327263+NguyenVu01@users.noreply.github.com>
2023-12-26 22:52:36 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+import sklearn
+
+# load model
+model = pickle.load(open('model.pkl', 'rb'))
+
+# create title
+st.title('Mô hình phân tích cảm xúc IMDB Reviews')
+
+review = st.text_input('Nhập vào review:')
+
+submit = st.button('Dự đoán')
+
+
+if submit:
+    prediction = model.predict([review])
+
+    if prediction[0] == 'positive':
+        st.success('Review Tích cực')
+    else:
+        st.warning('Review Tiêu cực')
+    st.balloons()
+
